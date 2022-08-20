5e43110164cbd939ddf478fc5e5a91e387b97f13
alexuioreanu <alexandru.uioreanu@yopeso.com>
2022-08-20 15:40:35 +0300

    Navigation with safe args :D

--- app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -23,6 +23,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
+import com.example.android.codelabs.navigation.FlowStepFragmentDirections.Companion.nextAction
 
 
 /**
@@ -41,6 +42,16 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
+        val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        button?.setOnClickListener {
+
+            findNavController().navigate(R.id.action_home_dest_to_flow_step_one_dest, null)
+        }
+
+
+        //TODO STEP 6 - Set NavOptions
         val options = navOptions {
             anim {
                 enter = R.anim.slide_in_right
@@ -49,33 +60,17 @@ class HomeFragment : Fragment() {
                 popExit = R.anim.slide_out_right
             }
         }
-
-        //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
-        val button = view.findViewById<Button>(R.id.navigate_destination_button)
-        button?.setOnClickListener {
-
-            findNavController().navigate(R.id.action_home_dest_to_flow_step_one_dest, null,options)
+        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, options)
         }
-
-
-        //TODO STEP 6 - Set NavOptions
-//        val options = navOptions {
-//            anim {
-//                enter = R.anim.slide_in_right
-//                exit = R.anim.slide_out_left
-//                popEnter = R.anim.slide_in_left
-//                popExit = R.anim.slide_out_right
-//            }
-//        }
-//        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest, null, options)
-//        }
         //TODO END STEP 6
 
         //TODO STEP 7.2 - Update the OnClickListener to navigate using an action
-//        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
-//                Navigation.createNavigateOnClickListener(R.id.next_action, null)
-//        )
+        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener {
+            val flowStepNumberArg = 1
+            val action = HomeFragmentDirections.actionHomeDestToFlowStepOneDest(flowStepNumberArg)
+            findNavController().navigate(action)
+    }
         //TODO END STEP 7.2
     }
 
